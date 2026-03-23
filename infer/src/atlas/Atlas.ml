3b1ea1a2ff0b6588a076cb4fe23841e87ce2be03
Jan Durec <durecjan223@gmail.com>
2026-03-23 21:10:12 +0100

    remove unused files

--- infer/src/atlas/Atlas.ml
+++ infer/src/atlas/Atlas.ml
@@ -2,7 +2,7 @@ open! IStd
 
 let checker (analysis_data : IntraproceduralAnalysis.t) : unit =
   let init_state = AtlasState.empty analysis_data in
-  let states, err_states = AtlasTransfer2.run analysis_data init_state in
+  let states, err_states = AtlasTransfer.run analysis_data init_state in
   let proc_name = Procdesc.get_proc_name analysis_data.proc_desc in
   Format.printf
     "@[<v2>Atlas finished procedure %a@]@."

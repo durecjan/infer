4a0f1c81c4a4910c0a9a5058a6dde1752b1bcf4e
Jan Durec <durecjan223@gmail.com>
2026-03-27 17:32:42 +0100

    switch analysis from using AtlasTransfer.run to AbstractInterpreter

--- infer/src/atlas/Atlas.ml
+++ infer/src/atlas/Atlas.ml
@@ -1,26 +1,38 @@
 open! IStd
 
+module Analyzer = AbstractInterpreter.MakeRPO (AtlasTransfer.TransferFunctions)
+
 let checker (analysis_data : IntraproceduralAnalysis.t) : unit =
   let init_state = AtlasState.empty analysis_data in
-  let final_states, err_states = AtlasTransfer.run analysis_data init_state in
+  let initial = [init_state] in
   let proc_name = Procdesc.get_proc_name analysis_data.proc_desc in
-  Format.printf
-    "@[<v2>Atlas finished procedure %a@]@."
-    Procname.pp proc_name;
-
-  Format.print_string (
-    "\n================\n" ^
-    "[FINAL STATES] :\n" ^
-    "================\n" ^
-    String.concat (
-      List.map
-        ~f:(fun state -> AtlasState.to_string state)
-        final_states));
-  Format.print_string (
-    "[ERROR STATES] :\n" ^
-    "================\n" ^
-    String.concat (
-      List.map
-        ~f:(fun state -> AtlasState.to_string state)
-        err_states));
-
+  match Analyzer.compute_post analysis_data ~initial analysis_data.proc_desc with
+  | Some final_domain ->
+    let final_states, err_states =
+      List.partition_tf final_domain ~f:(fun s ->
+        match s.AtlasState.status with
+        | AtlasState.Ok -> true
+        | AtlasState.Error _ -> false)
+    in
+    Format.printf
+      "@[<v2>Atlas finished procedure %a@]@."
+      Procname.pp proc_name;
+    Format.print_string (
+      "\n================\n" ^
+      "[FINAL STATES] :\n" ^
+      "================\n" ^
+      String.concat (
+        List.map
+          ~f:(fun state -> AtlasState.to_string state)
+          final_states));
+    Format.print_string (
+      "[ERROR STATES] :\n" ^
+      "================\n" ^
+      String.concat (
+        List.map
+          ~f:(fun state -> AtlasState.to_string state)
+          err_states))
+  | None ->
+    Format.printf
+      "Atlas: compute_post returned None for %a@."
+      Procname.pp proc_name

e7fb5d8f08346463e9a2679b622739ccacec15b5
Jan Durec <durecjan223@gmail.com>
2026-03-28 23:57:22 +0100

    update test cases 22,23 with expected behavior

--- infer/src/atlas/tests/22_queue_enqueue.c
+++ infer/src/atlas/tests/22_queue_enqueue.c
@@ -16,86 +16,24 @@ void enqueue(struct queue *q, char *data) {
     }
 }
 
-
 /*
-
-BUG
-
-description:
-flow is correct, on line (if (n == NULL) return;), we filter out failed malloc state, at line (if (q->tail == NULL)) we do not filter out any state since the solver result is Uknown and we add the condition to state.missing.pure of the respective states - there are also no duplicated intermediate debug prints, suggesting this is not a issue of exec_instr, however at the end of the debug prints, there is this:
-
-[SIL_METADATA]: APPLY_ABSTRACTION; [line 15, column 9]
-[SIL_METADATA]: APPLY_ABSTRACTION; [line 10, column 5]
-[SIL_METADATA]: APPLY_ABSTRACTION; [line 10, column 5]
-[SIL_METADATA]: APPLY_ABSTRACTION; [line 10, column 5]
-[SIL_METADATA]: APPLY_ABSTRACTION; [line 10, column 5]
-[SIL_METADATA]: APPLY_ABSTRACTION; [line 10, column 5]
-[SIL_METADATA]: APPLY_ABSTRACTION; [line 10, column 5]
-[SIL_METADATA]: APPLY_ABSTRACTION; [line 10, column 5]
-[SIL_METADATA]: APPLY_ABSTRACTION; [line 10, column 5]
-
-and within the debug prints, there is this:
-
-[SIL_METADATA]: APPLY_ABSTRACTION; [line 5, column 20]
-[SIL_METADATA]: APPLY_ABSTRACTION; [line 12, column 9]
-
-so it would seem that infer, via sil, is creating these duplicates that would be filtered out via join since they are equal (even in the aspect of cell variable ids) and we are not doing anything in AtlasTransfer.run to prevent this and with the current implementation of said method, we get 3 final states corresponding to the "malloc failed case", 8 final states corresponding to the "q->tail == NULL" case and 8 more final states corresponding to the "q->tail != NULL" case - all of them are complete duplicates and are equal in every aspect
-
-PROCFG GRAPH TRACE :
-
-void enqueue(struct queue *q, char *data) { S_1 summary for enqueue
-4	    struct node *n = (struct node *)malloc(sizeof(struct node)); I_19
-5	    if (n == NULL) return; I_15 C_16 I_18 C_17 J_14
-6
-7	    n->data = data; I_13
-8	    n->next = NULL; I_12
-9
-10	    if (q->tail == NULL) { I_5 C_6 C_7 J_3 I_4
-11	        q->head = n; I_9
-12	        q->tail = n; I_8
-13	    } else {
-14	        q->tail->next = n; I_11
-15	        q->tail = n; I_10
-16	    }
-17	} E_2
-
-node1   preds:      succs:19    exn: Start
-
-[struct node *n = (struct node *)malloc(sizeof(struct node));]
-node19  preds:1     succs:15    exn: Instructions
-
-[if (n == NULL) return;]
-node15  preds:19    succs:16 17 exn: Instructions
-node16  preds:15    succs:18    exn: Conditional true Branch
-node18  preds:16    succs:2     exn: Instructions
-node17  preds:15    succs:14    exn: Conditional false Branch
-node14  preds:17    succs:13    exn: Join
-
-[n->data = data;]
-node13  preds:14    succs:12    exn: Instructions
-
-[n->next = NULL;]
-node12  preds:13    succs:5     exn: Instructions
-
-[if (q->tail == NULL)]
-node5   preds:12    succs:6 7   exn: Instructions
-node6   preds:5     succs:9     exn: Conditional true Branch
-node7   preds:5     succs:11    exn: Conditional false Branch
-node3   preds:10 8  succs:4     exn: Join
-node4   preds:3     succs:2     exn: Instructions
-
-[q->head = n;]
-node9   preds:6     succs:8     exn: Instructions
-
-[q->tail = n;]
-node8   preds:9     succs:3     exn: Instructions
-
-[q->tail->next = n;]
-node11  preds:7     succs:10    exn: Instructions
-
-[q->tail = n;]
-node10  preds:11    succs:3     exn: Instructions
-
-
-
-*/
+ * Should produce exactly three OK final states:
+ * 1) n == NULL
+ *
+ * 2) n != NULL && q->tail == NULL
+ *      missing.spatial: (q) -(8)-> (x1) * (q+8) -(8)-> (x2)
+ *      missing.pure: (Base(q)<=q) & (End(q)>=(q+16)) & (x2==null)
+ *      current.spatial: (q) -(8)-> (n) * (q+8) -(8)-> (n) * (n) -(8)-> (data) * (n+8) -(8)-> (x3)
+ *      current.pure: (Base(n)==n) & (End(n)==n+16) & (x3==null) & (Base(x3)==0) & (End(x3)==0)
+ *
+ * 3) n!= NULL && q->tail != NULL
+ *      missing.spatial: (q+8) -(8)-> (x4) * (x4+8) -(8)-> x5
+ *      missing.pure: (Base(q)<=q) & (End(q)>=(q+16)) & (x4!=null) & (Base(x4)<=x4) & (End(x4)>=x4+16)
+ *      current.spatial: (q+8) -(8)-> (n) * (n) -(8)-> (data) * (n+8) -(8)-> (x6) * (x4+8) -(8)-> (n)
+ *      current.pure: (Base(n)==n) & (End(n)==n+16) & (x6==null) & (Base(x6)==0) & (End(x6)==0)
+ *
+ * and exactly three ERROR states:
+ * NullPointerDereference -- if (q->tail == NULL) [line 10, column 9]
+ * NullPointerDereference -- q->head = n; [line 11, column 9]
+ * NullPointerDereference -- q->tail->next = n; [line 14, column 9]
+ */
--- infer/src/atlas/tests/23_queue_dequeue.c
+++ infer/src/atlas/tests/23_queue_dequeue.c
@@ -14,3 +14,30 @@ char *dequeue(struct queue *q) {
     free(n);
     return data;
 }
+
+/*
+ * Should produce exactly three OK final states:
+ * 1) q->head == NULL
+ *      missing.spatial: (q) -(8)-> (x1)
+ *      missing.pure: (Base(q)<=q) & (End(q)>=(q+8)) & (x1==null)
+ *      current.spatial:
+ *      current.pure: (Base(%ret)==0) & (End(%ret)==0) & (%ret==null)
+ *
+ * 2) q->head != NULL && q->head->next == NULL
+ *      missing.spatial: (q) -(8)-> (x2) * (q+8) -(8)-> (x3) * (x2) -(8)-> (x4) * (x2+8) -(8)-> (x5)
+ *      missing.pure: (Base(q)<=q) & (End(q)>=q+16) & (x2!=null) & (Base(x2)<=x2) & (End(x2)>=x2+16) & (x5==null)
+ *      current.spatial: (q) -(8)-> (x5) * (q+8) -(8)-> (x6)
+ *      current.pure: Freed(x2) & (x6==null) & (Base(x6)==0) & (End(x6)==0) & (%ret==x4)
+ *
+ * 3) q->head != NULL && q->head->next != NULL
+ *      missing.spatial: (q) -(8)-> (x7) * (x7) -(8)-> (x8) * (x7+8) -(8)-> (x9)
+ *      missing.pure: (Base(q)<=q) & (End(q)>=q+16) & (x7!=null) & (Base(x7)<=x7) & (End(x7)>=x7+16) & (x9!=null)
+ *      current.spatial: (q) -(8)-> (x9)
+ *      current.pure: Freed(x7) & (%ret==x8)
+ *
+ * and exactly four ERROR states:
+ * NullPointerDereference -- if (q->head == NULL) [line 4, column 9]
+ * NullPointerDereference -- char *data = n->data; [line 7, column 18]
+ * NullPointerDereference -- q->head = n->next; [line 9, column 15]
+ * NullPointerDereference -- q->tail = NULL; [line 11, column 9]
+ */

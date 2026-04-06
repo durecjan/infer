a0736e57d9915bd3f1ab093def24eaae1dbd4fe2
Jan Durec <durecjan223@gmail.com>
2026-04-06 15:04:57 +0200

    [Tests] update TC 24,25 expected behavior

--- infer/src/atlas/tests/24_queue_destroy.c
+++ infer/src/atlas/tests/24_queue_destroy.c
@@ -11,3 +11,62 @@ void destroy(struct queue *q) {
     q->head = NULL;
     q->tail = NULL;
 }
+
+
+/*
+ * Ok: case q->head == NULL
+ * Expected precondition:
+ * (q + 0) -(8)-> x1 * (q + 8) -(8)-> x2
+ * (Base(q)<=q) & (End(q)>=q+16) & (x1==null)
+ * Expected postcondition:
+ * (q + 0) -(8)-> x3 * (q + 8) -(8)-> x4
+ * (Base(q)<=q) & (End(q)>=q+16) & (x3==null) & (Base(x3)==0) & (End(x3)==0) & (x4==null) & (Base(x4)==0) & (End(x4)==0)
+ *
+ * Ok: case q->head != NULL & q->head->data != NULL & q->head->next == NULL
+ * Expected precondition:
+ * (q + 0) -(8)-> x1 * (q + 8) -(8)-> x2 * (x1 + 0) -(8)-> x3 * (x1 + 8) -(8)-> x4 * (x3 + 0) -(1)-> block
+ * (Base(q)<=q) & (End(q)>=q+16) & (x1!=null) & (Base(x1)==x1) & (End(x1)>=x1+16) & (Base(x3)==x3) & (End(x3)>=x3+1) & (x4==null)
+ * Expected postcondition:
+ * (q + 0) -(8)-> x5 * (q + 8) -(8)-> x6
+ * (Base(q)<=q) & (End(q)>=q+16) & (x1!=null) & Freed(x1) & Freed(x3) & (x4==null) & (x5==null) & (Base(x5)==0) & (End(x)==0) & (x6==null) & (Base(x6)==0) & (End(x6)==0)
+ *
+ * Error: case q->head == NULL
+ * [line 4] Expected precondition:
+ * (Base(q)>q)
+ * [line 4] Expected precondition:
+ * (End(q)<q+8)
+ * [line 4] Expected precondition:
+ * Freed(q)
+ * [line 12] Expected precondition:
+ * (q + 0) -(8)-> x1
+ * (Base(q)<=q) & (End(q)>=q+8) & (x1==null) & (End(q)<q+16)
+ *
+ * Error: case q->head != NULL & q->head->data != NULL & q->head->next == NULL
+ * [line 4] Expected precondition:
+ * (Base(q)>q)
+ * [line 4] Expected precondition:
+ * (End(q)<q+8)
+ * [line 4] Expected precondition:
+ * Freed(q)
+ * [line 6] Expected precondition:
+ * (q + 0) -(8)-> x1
+ * (Base(q)<=q) & (End(q)>=q+8) & (x1!=null) & (Base(x1)>x1+8)
+ * [line 6] Expected precondition:
+ * (q + 0) -(8)-> x1
+ * (Base(q)<=q) & (End(q)>=q+8) & (x1!=null) & (End(x1)<x1+16)
+ * [line 6] Expected precondition:
+ * (q + 0) -(8)-> x1
+ * (Base(q)<=q) & (End(q)>=q+8) & (x1!=null) & (Freed(x1))
+ * [line 7] Expected precondition:
+ * (q + 0) -(8)-> x1 * (x1 + 8) -(8)-> x2
+ * (Base(q)<=q) & (End(q)>=q+8) & (x1!=null) & (Base(x1)<=x1+8) & (End(x1)>=x1+16) & (Base(x1)>x1)
+ * [line 7] Expected precondition:
+ * (q + 0) -(8)-> x1 * (x1 + 8) -(8)-> x2 * (x1 + 0) -(8)-> x3
+ * (Base(q)<=q) & (End(q)>=q+8) & (x1!=null) & (Base(x1)<=x1) & (End(x1)>=x1+16) & (Base(x3)!=x3)
+ * [line 8] Expected precondition:
+ * (q + 0) -(8)-> x1 * (x1 + 8) -(8)-> x2 * (x1 + 0) -(8)-> x3
+ * (Base(q)<=q) & (End(q)>=q+8) & (x1!=null) & (Base(x1)<=x1) & (End(x1)>=x1+16) & (Base(x3)==x3) & (End(x3)>=x3+1) & (Base(x1)!=x1)
+ * [line 12] Expected precondition:
+ * (q + 0) -(8)-> x1 * (x1 + 8) -(8)-> x2 * (x1 + 0) -(8)-> x3
+ * (Base(q)<=q) & (End(q)>=q+8) & (x1!=null) & (Base(x1)==x1) & (End(x1)>=x1+16) & (Base(x3)==x3) & (End(x3)>=x3+1) & (x2==null) & (End(q)<q+16)
+ */
--- infer/src/atlas/tests/25_dll_insert_head.c
+++ infer/src/atlas/tests/25_dll_insert_head.c
@@ -15,34 +15,50 @@ void dll_insert_head(struct dlist *dl, int val) {
 }
 
 /*
- * CASE if(n == NULL)
+ * Ok case n == NULL
  * Expected precondition:
- * <empty>
- * <empty>
  * Expected postcondition:
- * <empty>
  * (n==null) & (Base(n)==0) & (End(n)==0)
  *
- * CASE if (dl->head == NULL)
+ * Ok case n!= NULL && dl->head == NULL
  * Expected precondition:
- * (dl+0) -(8)-> ([z1])
- * (Base(dl)<=dl+0) & (End(dl)>=dl+8) & ([z1]==null)
+ * (dl+0) -(8)-> x1
+ * (Base(dl)<=dl+0) & (End(dl)>=dl+8) & (x1==null)
  * Expected postcondition:
- * if (dl->head == NULL)
- * (dl+0) -(8)-> (n) * (n+0) -(4)-> (val) * (n+4) -(8)-> (x1) * (n+12) -(8)-> ([z1]) * (n+20) -(4)-> block
- * (Base(n)==n) & (End(n)==n+24) & (x1==null) & (Base(x1)==0) & (End(x1)==0)
+ * (dl+0) -(8)-> x5 * (n+0) -(4)-> x2 * (n+4) -(8)-> x3 * (n+12) -(8)-> x4 * (n+20) -(4)-> block
+ * (Base(dl)<=dl+0) & (End(dl)>=dl+8) & (x1==null) & (Base(n)==n+0) & (End(n)==n+24) & (x2==val) & (x3==null) & (Base(x3)==0) & (End(x3)==0) & (x4==x1) & (x5==n)
  *
- * CASE if (dl->head != NULL)
+ * Error case n!= NULL && dl->head == NULL
+ * [line 9] Expected precondition:
+ * (Base(q)>q+0)
+ * [line 9] Expected precondition:
+ * (End(q)<q+8)
+ * [line 9] Expected precondition:
+ * Freed(q)
+ *
+ * Ok case n!=NULL && dl->head != NULL
  * Expected precondition:
- * (dl+0) -(8)-> ([z1]) * ([z1]+4) -(8)-> (y1)
- * (Base(dl)<=dl+0) & (End(dl)>=dl+8) & ([z1]!=null) & (Base([z1])<=[z1]+4) & (End([z1])>=[z1]+12)
- * * Expected postcondition:
- * if (dl->head != NULL)
- * (dl+0) -(8)-> (n) * (n+0) -(4)-> (val) * (n+4) -(8)-> (x1) * (n+12) -(8)-> ([z1]) * (n+20) -(4)-> block * ([z1]+4) -(8)-> (n)
- * (Base(n)==n) & (End(n)==n+24) & (x1==null) & (Base(x1)==0) & (End(x1)==0)
+ * (dl+0) -(8)-> x1 * (x1+4) -(8)-> x2
+ * (Base(dl)<=dl+0) & (End(dl)>=dl+8) & (Base(x1)<=x1+4) & (End(x1)>=x1+12) & (x1!=null)
+ * Expected postcondition:
+ * (dl+0) -(8)-> x3 * (x1+4) -(8)-> x4 * (n+0) -(4)-> x5 * (n+4) -(8)-> x6 * (n+12) -(8)-> x7 * (n+20) -(4)-> block
+ * (Base(dl)<=dl+0) & (End(dl)>=dl+8) & (Base(x1)<=x1+4) & (End(x1)>=x1+12) & (x1!=null) & (Base(n)==n+0) & (End(n)==n+24) & (x5==val) & (x6==null) & (Base(x6)==0) & (End(x6)==null) & (x7==x1) & (x3==n) & (x4==n)
  *
+ * Error case n!= NULL && dl->head != NULL
+ * [line 9] Expected precondition:
+ * (Base(q)>q+0)
+ * [line 9] Expected precondition:
+ * (End(q)<q+8)
+ * [line 9] Expected precondition:
+ * Freed(q)
+ * [line 12] Expected precondition:
+ * (dl+0) -(8)-> x1
+ * (Base(dl)<=dl+0) & (End(dl)>=dl+8) & (x1!=null) & (Base(x1)>x1+4)
+ * [line 12] Expected precondition:
+ * (dl+0) -(8)-> x1
+ * (Base(dl)<=dl+0) & (End(dl)>=dl+8) & (x1!=null) & (End(x1)<x1+12)
+ * [line 12] Expected precondition:
+ * (dl+0) -(8)-> x1
+ * (Base(dl)<=dl+0) & (End(dl)>=dl+8) & (x1!=null) & Freed(x1)
  *
- * Expected error contracts:
- * NullPointerDereference -- n->next = dl->head; [line 9, column 15]
- * NullPointerDereference -- dl->head->prev = n; [line 12, colum 9]
  */
